f7fc1aa3e8524f23e7bc34fdfedf23307771a4dc
daveyhmariam <daveyhmariam@gmail.com>
2024-08-20 23:53:29 +0300

    js stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,24 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
+// Create the interface to read input from stdin and write to stdout
+const interFace = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-function stdIn() {
-  rl.question('Welcome to Holberton School, what is your name? ', (name) => {
+// Function to handle user input and display the message
+function askName() {
+  interFace.question('Welcome to Holberton School, what is your name?\n', (name) => {
     console.log(`Your name is: ${name}`);
-    rl.close();
+    // Close the interface after printing the name
+    interFace.close();
   });
 }
 
+// Handle process exit event to display closing message
 process.on('exit', () => {
   console.log('This important software is now closing');
 });
 
-stdIn()
+// Start asking for the name
+askName();
