804964fbbfc4995f1a701f2efc83c11055ba2807
daveyhmariam <daveyhmariam@gmail.com>
2024-08-21 23:01:23 +0300

    eslint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   const array = [];
-  
+
   Object.keys(report.allEmployees).forEach((key) => {
     array.push(...report.allEmployees[key]);
   });
