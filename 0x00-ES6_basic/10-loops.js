5af5fe1a9491cef5769d375021123f570a7b0a10
daveyhmariam <daveyhmariam@gmail.com>
2024-08-21 17:58:57 +0300

    for of loop es6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   let index = 0;
   for (const value of array) {
-    index  = array.indexOf(value)
     array[index] = appendString + value;
+    index++;
   }
   return array;
-  }
+}
