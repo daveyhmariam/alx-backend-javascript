ff764ce98edb84a05ab9162801dfbc9952fe4723
daveyhmariam <daveyhmariam@gmail.com>
2024-08-21 22:54:12 +0300

    using iterator

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let str = '';
+  let isFirst = true;
+  for (const element of reportWithIterator) {
+    if (!isFirst) {
+      str += ` | ${element}`;
+    }
+    else {
+      str += `${element}`
+      isFirst = false
+    };
+  };
+  return str;
+}
