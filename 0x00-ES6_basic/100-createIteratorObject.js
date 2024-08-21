fe6bcff47c9352af97bc86199e61d9541a195b2b
daveyhmariam <daveyhmariam@gmail.com>
2024-08-21 22:36:20 +0300

    parse object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
-    const array = []
-    Object.keys(report.allEmployees).forEach(key => {
-        array.push(...report.allEmployees[key])
-    });
-    return array[Symbol.iterator]();
+ const array = []
+  Object.keys(report.allEmployees).forEach(key => {
+  array.push(...report.allEmployees[key])
+  });
+  return array[Symbol.iterator]();
 }
