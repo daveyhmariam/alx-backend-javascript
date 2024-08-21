48e557eb1b292a47c37d5caf88dbe88d9aefc133
daveyhmariam <daveyhmariam@gmail.com>
2024-08-21 17:38:16 +0300

    style

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,7 @@
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-      income,
-      gdp,
-      capita,
-    };
-  
-    return budget;
-  }
-  
+  return {
+  income,
+  gdp,
+  capita,
+  };
+}
