d1f994fc8b882b29bfb35fa9b75a39013da502a3
daveyhmariam <daveyhmariam@gmail.com>
2024-08-24 23:26:05 +0300

    handleProfileSignup

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils.js'
+
+export default async function handleProfileSignup() {
+
+  try {
+    const photo = await uploadPhoto()
+    const user = await createUser()
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  }
+  catch(error) {
+    console.log('Signup system offline')
+  }
+}
