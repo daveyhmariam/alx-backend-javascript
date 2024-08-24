b502378401b9ad0d1eb4461e962c29d4c46002dc
daveyhmariam <daveyhmariam@gmail.com>
2024-08-24 19:16:41 +0300

    getResponseFromAPI

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   const prom =  new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: 'success' });
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
