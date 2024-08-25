09a3f8c8c152e95c29dae8ec27db5af9df09b3ef
daveyhmariam <daveyhmariam@gmail.com>
2024-08-25 14:30:28 +0300

    handle user signup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((results) => results.map(({ status, value, reason }) => ({
-        status,
-        value: status === 'fulfilled' ? value : `${reason}`,
-      }))
+      status,
+      value: status === 'fulfilled' ? value : `${reason}`,
+      })),
     );
 }
