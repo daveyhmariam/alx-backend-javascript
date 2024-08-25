d50ec1cfc47b191840b75a0a7b817825782175bd
daveyhmariam <daveyhmariam@gmail.com>
2024-08-25 12:00:45 +0300

    style fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-
   try {
-    const photo = await uploadPhoto()
-    const user = await createUser()
+    const photo = await uploadPhoto();
+    const user = await createUser();
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  }
-  catch(error) {
+  } catch(error) {
     console.log('Signup system offline')
-  }
+  };
 }
