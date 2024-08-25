cc524739fd4a8ebc1695d34eedefb9c8744e2baf
daveyhmariam <daveyhmariam@gmail.com>
2024-08-25 14:20:23 +0300

    handle user signup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,17 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
-  const result = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(filename)
-  ])
-  return result.map(({ status, value, reason }) => {
-    status:
-    value: status === 'fulfilled' ? value : `${reason}`
-  });
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((results) => {
+      const result = [];
+      results.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          result.push({status: value.status, value: value.value});
+        } else {
+          result.push({status: value.status, value: `${value.reason}`});
+        }
+      });
+      return result;
+    });
 }
