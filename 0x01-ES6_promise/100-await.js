af39bfa4d009a46177370535affd385b49d8ab50
daveyhmariam <daveyhmariam@gmail.com>
2024-08-25 23:50:52 +0300

    asyncUploadUser

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,18 @@
-import uploadPhoto from './utils'
-import createUser from './utils'
+import uploadPhoto from './utils';
+import createUser from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const [photo, user] = await Promise.all([createUser(), uploadPhoto()]);
+    // Call both functions asynchronously
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    
+    // Return an object with the results
     return {
       photo,
       user,
     };
   } catch (error) {
+    // If any of the functions fail, return an object with null values
     return {
       photo: null,
       user: null,
