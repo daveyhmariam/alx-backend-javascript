2c4f12a3d28f8eea22d1f0e3548e31c92f25cf00
daveyhmariam <daveyhmariam@gmail.com>
2024-09-06 13:33:32 +0300

    array creation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const arr = [
+    {id: 1, firstName: 'Guillaume', location: 'San Fransisco'},
+    {id: 2, firstName: 'James', location: 'Columbia'},
+    {id: 3, firstName: 'Serena', location: 'San Fransisco'}
+  ]
+  return arr;
+}
