901a939303c80844377bacd996175ad1b55f543c
daveyhmariam <daveyhmariam@gmail.com>
2024-09-07 12:42:39 +0300

    linting

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ const createInt8TypedArray = (length, position, value) => {
   }
   const buf = new ArrayBuffer(length);
   const int8View = new Int8Array(buf, 0, length);
-  int8View.set([value], position)
+  int8View.set([value], position);
   return new DataView(buf);
 };
 
